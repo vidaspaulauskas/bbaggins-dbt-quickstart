a4515d76c6362fb7ec2088cffbfc0e883f24034d
vidas.paulauskas@exacaster.com <vidas.paulauskas@exacaster.com>
2024-04-09 19:48:03 +0000

    Model update

--- models/dim_customers.sql
+++ models/dim_customers.sql
@@ -1,59 +1,48 @@
-{{ config (
-    materialized="table"
-)}}
+{{ config(materialized="table") }}
 
-with customers as (
+with
+    customers as (
 
-    select
-        id as customer_id,
-        first_name,
-        last_name
+        select * from {{ ref('stg_customers') }}
 
-    from `dbt-tutorial`.jaffle_shop.customers
+    ),
 
-),
+    orders as (
 
-orders as (
+        select * from {{ ref('stg_orders') }}
 
-    select
-        id as order_id,
-        user_id as customer_id,
-        order_date,
-        status
+    ),
 
-    from `dbt-tutorial`.jaffle_shop.orders
+    customer_orders as (
 
-),
+        select
+            customer_id,
 
-customer_orders as (
+            min(order_date) as first_order_date,
+            max(order_date) as most_recent_order_date,
+            count(order_id) as number_of_orders
 
-    select
-        customer_id,
+        from orders
 
-        min(order_date) as first_order_date,
-        max(order_date) as most_recent_order_date,
-        count(order_id) as number_of_orders
+        group by 1
 
-    from orders
+    ),
 
-    group by 1
+    final as (
 
-),
+        select
+            customers.customer_id,
+            customers.first_name,
+            customers.last_name,
+            customer_orders.first_order_date,
+            customer_orders.most_recent_order_date,
+            coalesce(customer_orders.number_of_orders, 0) as number_of_orders
 
-final as (
+        from customers
 
-    select
-        customers.customer_id,
-        customers.first_name,
-        customers.last_name,
-        customer_orders.first_order_date,
-        customer_orders.most_recent_order_date,
-        coalesce(customer_orders.number_of_orders, 0) as number_of_orders
+        left join customer_orders using (customer_id)
 
-    from customers
+    )
 
-    left join customer_orders using (customer_id)
-
-)
-
-select * from final
+select *
+from final
