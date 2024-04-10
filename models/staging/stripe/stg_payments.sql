a444521dd3cb24606103b27099a995ea7059298b
Vidas Paulauskas <vidas.paulauskas@exacaster.com>
2024-04-10 11:04:48 +0000

    updates

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -2,15 +2,16 @@ with
     payments as (
 
         select 
-            id as customer_id,
+            id as payment_id,
             orderid as order_id,
             paymentmethod as payment_method,
             status,
-            amount,
-            created as creation_dt
+
+            -- amount is stored in cents, convert it to dollars
+            amount / 100 as amount,
+            created as created_at
 
         from `dbt-tutorial`.stripe.payment
-
     )
 
 SELECT * FROM payments
