d3c0bece7cc681779da7cd6ebba99aa1cf687bc6
Vidas Paulauskas <vidas.paulauskas@exacaster.com>
2024-04-10 07:15:10 +0000

    testing

--- models/staging/jaffle_shop/stg_customers.sql
+++ models/staging/jaffle_shop/stg_customers.sql
@@ -1,7 +1,7 @@
 with
     customers as (
 
-        select id as customer_id, first_name, last_name
+        select id as customer_id, first_name, last_name, 1 test
 
         from `dbt-tutorial`.jaffle_shop.customers
 
